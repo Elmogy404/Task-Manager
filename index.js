81b8443f16682f3f0112b45b01fe523527e71eb9
Elmogy404 <mohomedelmogy@gmail.com>
2026-08-27 02:23:35 +0300

    basic error handler

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 const initDB = require("./db/init");
 const tasks = require("./routes/tasks.js");
+const errorHandler = require("./middleware/errorHandler");
 
 app.use(express.json());
 
@@ -45,6 +46,7 @@ const specs = swaggerJsdoc(options);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/tasks", tasks);
 
+app.use(errorHandler);
 app.get("/", (req, res) => {
   res.json({
     name: "Task API",
