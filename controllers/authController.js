907a204a10bbd3348d7ba899cfe234debaddf237
Elmogy404 <mohomedelmogy@gmail.com>
2026-09-04 15:40:24 +0300

    JWT added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,38 +1,40 @@
 const bcrypt = require("bcrypt");
 const pool = require("../db/pool");
-const { use } = require("react");
+const jwt = require("jsonwebtoken");
+
+function validate(email, password) {
+  if (!email && !password) {
+    return "Email and password are required";
+  }
+  if (!email) {
+    return "Email is required";
+  }
+  if (!password) {
+    return "Password is required";
+  }
+  return null;
+}
 
 async function signup(req, res) {
   const { email, password } = req.body;
-  if (!email || !password) {
-    if (!email) {
-      return res.status(400).json({ error: "Email is required" });
-    }
-    if (!password) {
-      return res.status(400).json({ error: "Password is required" });
-    }
-    return res.status(400).json({ error: "Email and password are required" });
+  const error = validate(email, password);
+  if (error) {
+    return res.status(400).json({ error });
   }
-
-  const password_hash = await bcrypt.hash(password, 10);
+  const passwordHash = await bcrypt.hash(password, 10);
   const result = await pool.query(
     `INSERT INTO users (email, password_hash)
         VALUES ($1, $2) RETURNING id, email`,
-    [email, password_hash],
+    [email, passwordHash],
   );
   res.status(201).json(result.rows[0]);
 }
 
 async function login(req, res) {
   const { email, password } = req.body;
-  if (!email || !password) {
-    if (!email) {
-      return res.status(400).json({ error: "Email is required" });
-    }
-    if (!password) {
-      return res.status(400).json({ error: "Password is required" });
-    }
-    return res.status(400).json({ error: "Email and password are required" });
+  const error = validate(email, password);
+  if (error) {
+    return res.status(400).json({ error });
   }
   const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [
     email,
@@ -49,12 +51,17 @@ async function login(req, res) {
       error: "Invalid email or password",
     });
   }
-  res.status(200).json({
-    message: "Login Successful",
-    user: {
-      id: user.id,
+  const token = jwt.sign(
+    {
+      userId: user.id,
       email: user.email,
     },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" },
+  );
+  res.status(200).json({
+    message: "Login Successful",
+    token,
   });
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { signup, login } = require("../controllers/authController");
+const authMiddleware = require("../middleware/auth");
+
+router.get("/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "You are authenticated",
+    user: req.user,
+  });
+});
 
 router.post("/signup", signup);
 router.post("/login", login);
