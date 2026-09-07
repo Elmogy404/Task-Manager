6e5887277974ef58f447c51c44749ad99245bf8e
Elmogy404 <mohomedelmogy@gmail.com>
2026-09-08 00:27:00 +0300

    feat: add Dockerfile, fly.toml for Fly.io deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 initDB()
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log("Server is running on http://localhost:3000");
     });
   })
